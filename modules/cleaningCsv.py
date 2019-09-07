da9dd3f1126af9e695bad7a8beee4b8ab9132a31
Curro <farc@outlook.com>
2019-09-07 18:27:17 +0200

    add env

--- modules/cleaningCsv.py
+++ modules/cleaningCsv.py
@@ -1,17 +1,13 @@
 def cleaningCsv():
     import pandas as pd
+    from getEnv import getVariable
+    listCountry = getVariable("listCountry")
+    listYear = getVariable("listYear")
     df = pd.read_csv("../inputs/world_pop.csv")
-    df = df[['Country Code','Country','1990', '1991', '1992', '1993', '1994', '1995', '1996',
-       '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005',
-       '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014',
-       '2015', '2016']]
-    df = df.loc[df['Country'].isin(["Germany","France","UK","Italy","Spain","Poland","Romania","Netherlands","Belgium","Greece"])]
+    df = df[['Country Code','Country']+listYear]
+    df = df.loc[df['Country'].isin(listCountry)]
     df=df.reset_index()
-    listaY= ['1990', '1991', '1992', '1993', '1994', '1995', '1996',
-       '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005',
-       '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014',
-       '2015', '2016']
-    df["PopAvg"]=df[listaY].mean(axis = 1)
+    df["PopAvg"]=df[listYear].mean(axis = 1)
     df["PopAvg"]= df["PopAvg"].apply(lambda num:int(round(num,0)))    
     df = df.sort_values(by=['PopAvg'],ascending=False).reset_index()[['Country Code', 'Country','PopAvg']]
     return df
