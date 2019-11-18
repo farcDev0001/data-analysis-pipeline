0d575e7a05b102884f9cb38c093d267a652f570f
farc <farc@outlook.es>
2019-11-18 12:31:54 +0100

    getEnv.py optimized

--- modules/getEnv.py
+++ modules/getEnv.py
@@ -1,10 +1,23 @@
+from dotenv import load_dotenv
+import os
+
 def getVnames():
+    """
+    Auxiliar para saber los nombres de las variables de entorno 
+    return:
+        List con los nombres de las variables de entorno
+    """
     return ['listCountry','listYear','listCountryCode']
 
 def getVariable(variableName):
-    from dotenv import load_dotenv
+    """
+    Carga la valiable de entorno de alchivo .env en la raíz del proyecto y la devuelve en forma de string
+    args:
+        String con la variable de entorno que se quiere cargar
+    return:
+        String con la variable de entorno
+    """
     load_dotenv("../.env")
-    import os
     return os.getenv(variableName).split(" ")
 
 
