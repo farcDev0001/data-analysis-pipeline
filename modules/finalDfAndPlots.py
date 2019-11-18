fc7141d241b7accbe0b94535caca68291b420cca
farc <farc@outlook.es>
2019-11-18 11:54:49 +0100

    finalDfAndPlots.py optimized

--- modules/finalDfAndPlots.py
+++ modules/finalDfAndPlots.py
@@ -1,6 +1,18 @@
+import pandas as pd
+from mergeDf import getMergedDf
+import matplotlib.pyplot as plt
+from reportPdf import exportRep
+from reportPdf import exportRep
+from getEnv import getVariable
+
 def getFinalCountryDf(countryCode):
-    import pandas as pd
-    from mergeDf import getMergedDf
+    """
+    Devuelve el cuadro de datos de un país a partir de su código
+    args: 
+        countryCode:Código de país
+    return:
+        df: Pandas DataFrame
+    """
     df = getMergedDf()
     df = df.loc[df['Country Code']==countryCode]
     df['discharges/10**5hab']= df['discharges/10**5hab'].apply(lambda ele: ele/100)
@@ -12,8 +24,13 @@ def getFinalCountryDf(countryCode):
     return df
 
 def getFinalYearDf(year):
-    import pandas as pd
-    from mergeDf import getMergedDf
+    """
+    Devuelve el cuadro de datos de un año
+    args: 
+        year: Integer Año
+    return:
+        df: Pandas DataFrame
+    """
     df = getMergedDf()
     df = df.loc[df['YEAR']==year]
     df['discharges/10**5hab']= df['discharges/10**5hab'].apply(lambda ele: ele/100)
@@ -22,53 +39,59 @@ def getFinalYearDf(year):
     df = df.reset_index()
     df = df[['Country Code', 'dischargesPer1000hab','tonnesPerCap']]
     getPlotbyYear(df,year,'Country Code', 'dischargesPer1000hab','tonnesPerCap')
-    
     return df
 
 def getPlotbyCountry(df,ccode,x,y1,y2,full = False):
-    import matplotlib.pyplot as plt
+    """
+    Muestra el gráfico por país, guarda el informe en pdf y y el gráfico en png 
+    args: 
+        df: Pandas DataFrame con los datos de país
+        ccode: String con el código del país
+        x: String con el nombre del eje de abscisas
+        y1: String con el nombre de la linea de ingresos hospitalarios
+        y2: String con el nombre de la linea de contaminación
+        full: Bool, True si se se trata del fullReport, default False
+    return:
+        df: Pandas DataFrame
+    """
     dfret = df
     ax = plt.gca()
     df.plot(kind='line',x=x,y=y1,ax=ax)
     df.plot(kind='line',x=x,y=y2, color='red', ax=ax)
     plt.title("HOSPITAL DISCHARGES, MENTAL AND BEHAVIOURAL DISORDERS AND\nGREENHOUSE GAS EMISSIONS IN {}".format(ccode))
-    
     plt.savefig('./../outputs/{}.png'.format(ccode))
-
     plt.show()
-    
-    from reportPdf import exportRep
     if full:
         exportRep('./../outputs/full.pdf','./../outputs/{}.png'.format(ccode),'./../inputs/full.txt')      
-        
     else:
         exportRep('./../outputs/{}.pdf'.format(ccode),'./../outputs/{}.png'.format(ccode))
-    
-    
-
-    
     return dfret
 
 def getPlotbyYear(df,year,x,y1,y2):
-    import matplotlib.pyplot as plt
-    
+    """
+    Muestra el gráfico por año, guarda el informe en pdf y y el gráfico en png 
+    args: 
+        df: Pandas DataFrame con los datos del año
+        year: Integer con el año
+        x: String con el nombre del eje de abscisas
+        y1: String con el nombre de la barra de ingresos hospitalarios
+        y2: String con el nombre de la barra de contaminación
+        full: Bool, True si se se trata del fullReport, default False
+    return:
+        df: Pandas DataFrame
+    """
     df.plot(x=x, y=[y1,y2], kind="bar")
     plt.title("HOSPITAL DISCHARGES, MENTAL AND BEHAVIOURAL\nDISORDERS AND GREENHOUSE GAS EMISSIONS IN {}".format(year))
-    
     plt.savefig('./../outputs/{}.png'.format(year))
-
     plt.show()
-
-    from reportPdf import exportRep
     exportRep('./../outputs/{}.pdf'.format(year),'./../outputs/{}.png'.format(year))
 
-    
-
-
 def getFullYearReport():
-    import pandas as pd
-    from mergeDf import getMergedDf
-    from getEnv import getVariable
+    """
+    Muestra el gráfico de todos los años, guarda el informe en pdf y y el gráfico en png 
+    return:
+        df: Pandas DataFrame
+    """
     df = getMergedDf()
     df['discharges/10**5hab']= df['discharges/10**5hab'].apply(lambda ele: ele/100)
     df.columns = ['Country Code', 'Country', 'PopAvg', 'Year', 'dischargesPer1000hab','tonnesPerCap']
@@ -78,7 +101,7 @@ def getFullYearReport():
     df = df.reset_index()
     df = df[['Year', 'dischargesPer1000hab','tonnesPerCap']]
     df = df.groupby('Year').mean()
-    #Voy a quitar los outliers porque todos los paises no tienen registros todos los años
+    #Se quitan los outliers (valores extremos) porque todos los paises no tienen registros todos los años
     stats = df.describe().transpose()
     stats['IQR'] = stats['75%'] - stats['25%']
     outliers = pd.DataFrame(columns=df.columns)
